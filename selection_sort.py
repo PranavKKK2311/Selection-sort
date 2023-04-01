33d2341587b1a47a4ec16ba22620f6b7726d7e7c
PranavKKK2311 <124026011+PranavKKK2311@users.noreply.github.com>
2023-04-01 21:43:41 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,34 +1,3 @@
-# The selection sort algorithm sorts an array by repeatedly finding the minimum element.
-# Time Complexity :  O(n*n)
-
-"""
-Algorithm :-
-Step 1 − Set MIN to location 0
-Step 2 − Search the minimum element in the list
-Step 3 − Swap with value at location MIN
-Step 4 − Increment MIN to point to next element
-Step 5 − Repeat until list is sorted
-Pseudocode Selection Sort  :-
-   list  : array of items
-   n     : size of list
-   for i = 1 to n - 1
-   /* set current element as minimum*/
-      min = i
-
-      /* check the element to be minimum */
-      for j = i+1 to n
-         if list[j] < list[min] then
-            min = j;
-         end if
-      end for
-      /* swap the minimum element with the current element*/
-      if indexMin != i  then
-         swap list[min] and list[i]
-      end if
-   end for
-
-end procedure
-"""
 
 
 def selection_sort(alist):
